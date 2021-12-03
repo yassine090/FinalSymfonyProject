bc48ad71f4d802d8b1567e9bbdc44b6cd3d641d9
yassine090 <73793462+yassine090@users.noreply.github.com>
2021-12-03 01:58:12 +0100

    Add files via upload

---
+++ CheckUp/FormulaireType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Formulaire;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+/**@param string|null $type
+ */
+
+
+
+
+class FormulaireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Nom')
+            ->add('Prenom')
+            ->add('Adresse')
+            ->add('Sexe', ChoiceType::class, [
+                'choices' => [
+                    'Homme' => 'Homme',
+
+                    'Femme' => 'Femme',
+                ],'expanded'=> true
+            ])
+            ->add('Age')
+            ->add('DescriptionSymptomes')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Formulaire::class,
+        ]);
+    }
+}
